f797e41f3d66496fae378c59918f71488c16f4ba
Moses Chukwunekwu <moses@omenai.net>
2024-12-04 15:29:45 +0100

    Fixes

--- apps/server/middleware.ts
+++ apps/server/middleware.ts
@@ -1,66 +1,51 @@
 import { getSession } from "@omenai/shared-auth/lib/auth/session";
 import { NextRequest, NextResponse } from "next/server";
+
 const allowedOrigins = [
   "http://omenai.local:3000",
   "http://localhost:3000",
   "https://admin.omenai.app",
 ];
-const corsOptions = {
+
+const corsHeaders = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 export async function middleware(request: NextRequest) {
-  // Check the origin from the request
   const origin = request.headers.get("origin") ?? "";
+
+  // Check if the origin is allowed
   const isAllowedOrigin = allowedOrigins.includes(origin);
-  console.log(origin);
 
-  // Handle preflighted requests
-  const isPreflight = request.method === "OPTIONS";
-
-  if (isPreflight) {
-    const preflightHeaders = {
-      ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
-      ...corsOptions,
-    };
-    return NextResponse.json({}, { headers: preflightHeaders });
+  // Handle preflight requests
+  if (request.method === "OPTIONS") {
+    return NextResponse.json(
+      {},
+      {
+        headers: {
+          ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
+          ...corsHeaders,
+        },
+      }
+    );
   }
 
-  // Handle simple requests
+  // Handle other requests
   const response = NextResponse.next();
 
   if (isAllowedOrigin) {
     response.headers.set("Access-Control-Allow-Origin", origin);
   }
 
-  Object.entries(corsOptions).forEach(([key, value]) => {
+  Object.entries(corsHeaders).forEach(([key, value]) => {
     response.headers.set(key, value);
   });
 
   if (request.nextUrl.pathname.includes("/api/auth")) return response;
-  // else {
-  // }
-
-  // if (request.nextUrl.pathname === "/auth/login")
-  //   if (isMobileApp && isMobileApp === "Omenai-X-app/1.0.0") {
-  //     if (mobileAuth === process.env.APP_AUTHORIZATION_SECRET) {
-  //       return response;
-  //     } else {
-  //       return NextResponse.json(
-  //         { message: "Invalid Authorization signatures" },
-  //         { status: 403 }
-  //       );
-  //     }
-  //   }
 
   const session = await getSession();
   console.log(session);
-  // if (session === undefined)
-  //   return NextResponse.json(
-  //     { message: "Session expired, please login" },
-  //     { status: 403 }
-  //   );
 
   return response;
 }
