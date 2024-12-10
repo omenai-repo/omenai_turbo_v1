8cfdb260a99d3ab95a63dd2f68c3ac87d2e62c45
Moses Chukwunekwu <moses@omenai.net>
2024-12-10 16:44:58 +0100

    Updated global cors middleware

--- apps/server/middleware.ts
+++ apps/server/middleware.ts
@@ -1,43 +1,57 @@
 import { NextRequest, NextResponse } from "next/server";
-
+const allowed_origins = [
+  "https://auth.omenai.app",
+  "https://dashboard.omenai.app",
+  "https://admin.omenai.app",
+  "https://omenai.app",
+  "http://localhost",
+];
 export async function middleware(request: NextRequest) {
   // Handle other requests
   const response = NextResponse.next();
   const origin = request.headers.get("origin") ?? "";
-  const allowedOrigin =
-    process.env.NODE_ENV === "production" ? "https://*.omenai.app" : origin;
 
-  // Handle preflight (OPTIONS) requests
-  if (request.method === "OPTIONS") {
-    const preflightResponse = NextResponse.json({}, { status: 200 });
-    preflightResponse.headers.set("Access-Control-Allow-Origin", allowedOrigin);
-    preflightResponse.headers.set("Access-Control-Allow-Credentials", "true");
-    preflightResponse.headers.set(
-      "Access-Control-Allow-Methods",
-      "GET, DELETE, PATCH, POST, PUT, OPTIONS"
-    );
-    preflightResponse.headers.set(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, Content-Length"
-    );
-    return preflightResponse;
+  function isOriginAllowed(origin: string): boolean {
+    return allowed_origins.some((allowedOrigin) => {
+      return origin.startsWith(allowedOrigin);
+    });
   }
+  if (isOriginAllowed(origin)) {
+    // Handle preflight (OPTIONS) requests
+    if (request.method === "OPTIONS") {
+      const preflightResponse = NextResponse.json({}, { status: 200 });
+      preflightResponse.headers.set("Access-Control-Allow-Origin", origin);
+      preflightResponse.headers.set("Access-Control-Allow-Credentials", "true");
+      preflightResponse.headers.set(
+        "Access-Control-Allow-Methods",
+        "GET, DELETE, PATCH, POST, PUT, OPTIONS"
+      );
+      preflightResponse.headers.set(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, Content-Length"
+      );
+      return preflightResponse;
+    }
 
-  if (origin) {
-    response.headers.set("Access-Control-Allow-Credentials", "true");
-    response.headers.set("Access-Control-Allow-Origin", allowedOrigin);
-    response.headers.set(
-      "Access-Control-Allow-Methods",
-      "GET, DELETE, PATCH, POST, PUT, OPTIONS"
-    );
-    response.headers.set(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, Content-Length"
-    );
+    if (origin) {
+      response.headers.set("Access-Control-Allow-Credentials", "true");
+      response.headers.set("Access-Control-Allow-Origin", origin);
+      response.headers.set(
+        "Access-Control-Allow-Methods",
+        "GET, DELETE, PATCH, POST, PUT, OPTIONS"
+      );
+      response.headers.set(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, Content-Length"
+      );
+    }
+
+    if (request.nextUrl.pathname.includes("/api/auth")) return response;
+
+    return response;
+  } else {
+    return NextResponse.json({}, { status: 403 });
   }
-  if (request.nextUrl.pathname.includes("/api/auth")) return response;
-
-  return response;
 }
 
 export const config = {
