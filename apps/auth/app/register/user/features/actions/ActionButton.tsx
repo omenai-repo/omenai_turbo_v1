b912f7215d686a13c433f182b5980416cb409380
Moses Chukwunekwu <moses@omenai.net>
2025-03-14 17:16:22 +0100

    FIX

--- apps/auth/app/register/user/features/actions/ActionButton.tsx
+++ apps/auth/app/register/user/features/actions/ActionButton.tsx
@@ -113,7 +113,7 @@ export default function ({
       <button
         className={`${
           currentSignupFormIndex > 0 ? "block" : "hidden"
-        }  disabled:cursor-not-allowed bg-dark hover:bg-dark/80 text-white focus:ring ring-1 border-0 ring-dark/20 focus:ring-white duration-300 outline-none focus:outline-none disabled:bg-dark/50 disabled:text-white rounded-full h-[40px] p-5 sm:p-6 w-full text-center text-xs sm:text-[14px] flex items-center justify-center hover:ring-white cursor-pointer`}
+        }  bg-dark hover:bg-dark/80 disabled:cursor-not-allowed text-white focus:ring ring-1 border-0 ring-dark/20 focus:ring-white duration-300 outline-none focus:outline-none disabled:bg-dark/50 disabled:text-white rounded-full h-[40px] p-6 w-full text-center text-[14px] flex items-center justify-center hover:ring-white cursor-pointer`}
         type={"button"}
         onClick={handleClickPrev}
       >
