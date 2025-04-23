7aa2b2cf1a7edd3e4d70df217e5b035c735585aa
Moses Chukwunekwu <moses@omenai.net>
2025-04-23 22:06:53 +0100

    Minor fix

--- apps/server/app/api/workflows/shipment/create_shipment/route.ts
+++ apps/server/app/api/workflows/shipment/create_shipment/route.ts
@@ -7,21 +7,21 @@ import { getApiUrl } from "@omenai/url-config/src/config";
 import { sendBuyerShipmentEmail } from "@omenai/shared-emails/src/models/shipment/sendBuyerShipmentEmail";
 import { sendSellerShipmentEmail } from "@omenai/shared-emails/src/models/shipment/sendSellerShipmentEmail";
 import uploadWaybillDocument from "../resources";
-import { documentation_storage, storage } from "@omenai/appwrite-config";
+import { documentation_storage } from "@omenai/appwrite-config";
 
 const base64ToPDF = (base64: string, filename = "document.pdf"): File => {
-  const byteCharacters = atob(base64); // Decode base64 string
+  const byteCharacters = atob(base64);
   const byteNumbers = Array.from(byteCharacters).map((char) =>
     char.charCodeAt(0)
   );
   const byteArray = new Uint8Array(byteNumbers);
-
   return new File([byteArray], filename, { type: "application/pdf" });
 };
 
 type Payload = {
   order_id: string;
 };
+
 export const { POST } = serve<Payload>(async (ctx) => {
   const payload: Payload = ctx.requestPayload;
 
@@ -68,84 +68,86 @@ export const { POST } = serve<Payload>(async (ctx) => {
           body: JSON.stringify(data),
         }
       );
-      const result = await response.json();
-      console.log(result);
+
+      const responseData = await response.json();
+
       if (!response.ok) {
         return false;
-      } else {
-        const responseData = await response.json();
-        const trackingCode = responseData.data.shipmentTrackingNumber;
-        const waybillDocument = responseData.data.documents[0].content;
-        const file: File = base64ToPDF(waybillDocument, "waybilldoc.pdf");
-
-        // Upload waybill document
-        const uploadDoc = await uploadWaybillDocument(file);
-        let waybillDoc = "";
-        if (uploadDoc) {
-          let file: { bucketId: string; fileId: string } = {
-            bucketId: uploadDoc.bucketId,
-            fileId: uploadDoc.$id,
-          };
-
-          const waybillFile = documentation_storage.getFileView(
-            process.env.NEXT_PUBLIC_APPWRITE_DOCUMENTATION_BUCKET_ID!,
-            file.fileId
-          );
-          waybillDoc = waybillFile;
-        } else {
-          return NextResponse.json(
-            { data: "Problem with document upload" },
-            { status: 500 }
-          );
-        }
-
-        if (waybillDoc === "")
-          return NextResponse.json(
-            { data: "Problem with document upload" },
-            { status: 500 }
-          );
-        // Update DB with doc link
-        const updateOrder = await CreateOrder.updateOne(
-          { order_id: payload.order_id },
-          {
-            $set: {
-              "shipping_details.shipment_information.waybill_document":
-                waybillDoc,
-              "shipping_details.shipment_information.tracking.id": trackingCode,
-              "shipping_details.shipment_information.tracking.link":
-                "https://dashbard.omenai.app/orders",
-            },
-          }
-        );
-
-        if (updateOrder.modifiedCount === 0) {
-          return NextResponse.json(
-            { data: "Failed to update waybill document in DB" },
-            { status: 500 }
-          );
-        }
-
-        // Send mail
-        await sendBuyerShipmentEmail({
-          name: data.receiver_data.fullname,
-          email: data.receiver_data.email,
-          trackingCode,
-        });
-        await sendSellerShipmentEmail({
-          name: data.seller_details.fullname,
-          email: data.seller_details.email,
-          fileContent: waybillDoc || "",
-        });
-
-        return true;
       }
+
+      const trackingCode = responseData.data.shipmentTrackingNumber;
+      const waybillDocument = responseData.data.documents[0].content;
+      const file: File = base64ToPDF(waybillDocument, "waybilldoc.pdf");
+
+      const uploadDoc = await uploadWaybillDocument(file);
+      let waybillDoc = "";
+      if (uploadDoc) {
+        const fileData = {
+          bucketId: uploadDoc.bucketId,
+          fileId: uploadDoc.$id,
+        };
+
+        const waybillFile = documentation_storage.getFileView(
+          process.env.NEXT_PUBLIC_APPWRITE_DOCUMENTATION_BUCKET_ID!,
+          fileData.fileId
+        );
+        waybillDoc = waybillFile;
+      } else {
+        return NextResponse.json(
+          { data: "Problem with document upload" },
+          { status: 500 }
+        );
+      }
+
+      if (waybillDoc === "") {
+        return NextResponse.json(
+          { data: "Problem with document upload" },
+          { status: 500 }
+        );
+      }
+
+      const updateOrder = await CreateOrder.updateOne(
+        { order_id: payload.order_id },
+        {
+          $set: {
+            "shipping_details.shipment_information.waybill_document":
+              waybillDoc,
+            "shipping_details.shipment_information.tracking.id": trackingCode,
+            "shipping_details.shipment_information.tracking.link":
+              "https://dashbard.omenai.app/orders",
+          },
+        }
+      );
+
+      if (updateOrder.modifiedCount === 0) {
+        return NextResponse.json(
+          { data: "Failed to update waybill document in DB" },
+          { status: 500 }
+        );
+      }
+
+      await sendBuyerShipmentEmail({
+        name: data.receiver_data.fullname,
+        email: data.receiver_data.email,
+        trackingCode,
+      });
+
+      await sendSellerShipmentEmail({
+        name: data.seller_details.fullname,
+        email: data.seller_details.email,
+        fileContent: waybillDoc || "",
+      });
+
+      return true;
     }),
   ]);
-  if (create_shipment)
+
+  if (create_shipment) {
     return NextResponse.json({ data: "Successful" }, { status: 201 });
-  else
+  } else {
     return NextResponse.json(
       { data: "Failed to create shipment" },
       { status: 500 }
     );
+  }
 });
