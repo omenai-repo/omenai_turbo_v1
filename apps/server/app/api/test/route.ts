41307adaac98afeb48e30ae55f5390d8237e3adc
Moses Chukwunekwu <moses@omenai.net>
2025-10-23 15:25:46 +0100

    TEST_FIX

--- apps/server/app/api/test/route.ts
+++ apps/server/app/api/test/route.ts
@@ -1,7 +1,17 @@
 import { NextResponse } from "next/server";
 export async function GET() {
-  console.log("DHL_API_KEY loaded:", !!process.env.DHL_API_KEY);
-  console.log("DHL_API_SECRET loaded:", !!process.env.DHL_API_SECRET);
+  console.log(
+    "DHL_API_KEY loaded:",
+    !!process.env.DHL_API_KEY,
+    "key",
+    process.env.DHL_API_KEY
+  );
+  console.log(
+    "DHL_API_SECRET loaded:",
+    !!process.env.DHL_API_SECRET,
+    "secret",
+    process.env.DHL_API_SECRET
+  );
   return NextResponse.json({
     message: "Successful",
   });
