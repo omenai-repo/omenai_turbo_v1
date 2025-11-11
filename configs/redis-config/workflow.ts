54a842b1ef3d871175a2ac00fea74cc92a3962ab
Moses Chukwunekwu <moses@omenai.net>
2025-11-11 02:52:23 +0100

    FIX: Minor fix

--- configs/redis-config/workflow.ts
+++ configs/redis-config/workflow.ts
@@ -10,7 +10,7 @@ export async function createWorkflow(
 ) {
   try {
     const { workflowRunId } = await client.trigger({
-      url: `https://a673829c0f4a.ngrok-free.app${task_route}`,
+      url: `${getApiUrl()}${task_route}`,
       body: params,
       workflowRunId: workflowRun_id,
       headers: { Origin: "https://omenai.app" },
