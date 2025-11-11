f213b00f8098909d98f4ece27e770bfdbdf1012e
rodolphe-O <rodolphe@omenai.net>
2025-11-11 10:15:17 -0500

    Feat: blocked artist email

--- apps/server/app/api/test/route.ts
+++ apps/server/app/api/test/route.ts
@@ -2,11 +2,11 @@ import { sendBuyerShipmentEmail } from "@omenai/shared-emails/src/models/shipmen
 import { NextResponse } from "next/server";
 import { sendArtistShippmentSuccessfulMail } from "../../../../../shared/shared-emails/src/models/artist/sendArtistShippmentSuccessfulMail";
 import { sendGalleryShipmentSuccessfulMail } from "../../../../../shared/shared-emails/src/models/gallery/sendGalleryShipmentSuccessfulMail";
+import { sendArtistBlockedMail } from "@omenai/shared-emails/src/models/artist/sendArtistBlockedMail";
 export async function GET() {
-  const promise = await sendBuyerShipmentEmail({
+  const promise = await sendArtistBlockedMail({
     name: "Test User",
-    email: "moses@omenai.net",
-    trackingCode: "TEST123456",
+    email: "rodolphe@omenai.net",
   });
 
   return NextResponse.json({
