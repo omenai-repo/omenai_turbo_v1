b1cdf559d60629c6b77033e5282294fcf6faf3a2
rodolphe-O <rodolphe@omenai.net>
2025-12-05 12:28:44 -0500

    Feat: user dashboard v2

--- apps/dashboard/app/user-v2/modals/DeleteAccountConfirmationMdal.tsx
+++ apps/dashboard/app/user-v2/modals/DeleteAccountConfirmationMdal.tsx
@@ -14,9 +14,9 @@ export const DeleteAccountConfirmationModal = () => {
             updateDeleteUserAccountModalPopup(false);
           }}
           className="bg-slate-900/20 backdrop-blur py-8 px-2 fixed inset-0 z-50 grid place-items-center cursor-pointer"
+          aria-label="Close modal"
         >
           <div
-            role="button"
             onClick={(e) => e.stopPropagation()}
             className="bg-white text-dark p-6 rounded w-full max-w-3xl shadow-xl cursor-default relative h-auto"
           >
