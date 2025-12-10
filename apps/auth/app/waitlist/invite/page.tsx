8bdb32e4d3fd5ab7101ba8ee952c37ca2225e211
rodolphe-O <rodolphe@omenai.net>
2025-12-10 06:24:10 -0500

    Feat: waitlist registration

--- apps/auth/app/waitlist/invite/page.tsx
+++ apps/auth/app/waitlist/invite/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import InviteForm from "./InviteForm";
 import PageLayout from "../PageLayout";
+import { redirect } from "next/navigation";
 
 export default async function page({
   searchParams,
@@ -8,7 +9,7 @@ export default async function page({
   searchParams: Promise<{ entity: string | undefined }>;
 }) {
   const entity = (await searchParams).entity;
-  if (!entity) return;
+  if (!entity) redirect("/register");
   return (
     <PageLayout>
       <InviteForm entity={entity} />
