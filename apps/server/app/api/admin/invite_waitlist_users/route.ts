be95e31a8ee0e6534f75c308c67a9cedff609f65
rodolphe-O <rodolphe@omenai.net>
2025-12-17 10:47:50 -0500

    Update: invite waitlist users

--- apps/server/app/api/admin/invite_waitlist_users/route.ts
+++ apps/server/app/api/admin/invite_waitlist_users/route.ts
@@ -64,7 +64,7 @@ export const POST = withRateLimitHighlightAndCsrf(config)(async function POST(
     const bulkResult = await Waitlist.bulkWrite(bulkOps);
 
     // Validate bulk write results
-    const { matchedCount, modifiedCount } = bulkResult;
+    const { modifiedCount } = bulkResult;
     const bulkErrors = bulkResult.getWriteErrors();
 
     // Determine successfully updated users
