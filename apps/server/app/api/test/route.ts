883a014435b585557fdf6fafec01bbd6d7327835
rodolphe-O <rodolphe@omenai.net>
2026-01-15 12:51:12 -0500

    update: waitlist email content

--- apps/server/app/api/test/route.ts
+++ apps/server/app/api/test/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 import { generateInvoicePdf } from "@omenai/shared-lib/invoice/generateInvoice";
 import { mockInvoice } from "./test_workflow/p";
+import { SendWaitlistRegistrationEmail } from "@omenai/shared-emails/src/models/waitlist/SendWaitlistRegistrationEmail";
 
 export async function GET() {
-  const pdfBuffer = await generateInvoicePdf(mockInvoice);
+  await SendWaitlistRegistrationEmail({ email: "moses@omenai.net" });
 
   return NextResponse.json({
     message: "Successful",
-    isTrue: !!pdfBuffer,
   });
 }
