af928a6a486827d21336008488cd42cc3913638a
rodolphe-o <rodolphe@omenai.net>
2026-02-11 04:48:34 -0500

    Update: weight label

--- shared/shared-lib/validations/upload_artwork_input_validator/validateWeight.ts
+++ shared/shared-lib/validations/upload_artwork_input_validator/validateWeight.ts
@@ -8,13 +8,13 @@ export const validateWeight = (value: string): string[] => {
   // Validate if the value is not blank and follows the weight format
   if (
     !schema
-      .regex(/^\d+(\.\d+)?\s*(pounds?)$/i, {
+      .regex(/^\d+(\.\d+)?\s*(lbs?)$/i, {
         message: "Invalid weight format",
       })
       .safeParse(value).success
   ) {
     errors.push(
-      "Invalid weight detected 🚫. We need real measurements like '5pounds' or '25pounds', not vibes.",
+      "Invalid weight detected 🚫. We need real measurements like '5lbs' or '25lbs', not vibes.",
     );
   }
 
