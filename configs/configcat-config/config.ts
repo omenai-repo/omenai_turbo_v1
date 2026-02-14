c59a45b11803047d412384ecccbb92c7c326aebb
Moses Chukwunekwu <moses@omenai.net>
2026-02-14 23:52:18 +0100

    fix

--- configs/configcat-config/config.ts
+++ configs/configcat-config/config.ts
@@ -1,14 +1,8 @@
 import * as configcat from "@configcat/sdk/node";
 
-const highRiskSdkKey =
-  process.env.APP_ENV === "production"
-    ? (process.env.CONFIGCAT_HIGH_RISK_SDK_KEY as string)
-    : (process.env.CONFIGCAT_STAGING_HIGH_RISK_SDK_KEY as string);
+const highRiskSdkKey = process.env.CONFIGCAT_HIGH_RISK_SDK_KEY as string;
 
-const lowRiskSdkKey =
-  process.env.APP_ENV === "production"
-    ? (process.env.CONFIGCAT_LOW_RISK_SDK_KEY as string)
-    : (process.env.CONFIGCAT_STAGING_LOW_RISK_SDK_KEY as string);
+const lowRiskSdkKey = process.env.CONFIGCAT_LOW_RISK_SDK_KEY as string;
 
 const highRiskconfigCatClient = configcat.getClient(
   highRiskSdkKey,
