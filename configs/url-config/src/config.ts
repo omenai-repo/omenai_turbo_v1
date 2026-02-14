c1dcba9825213470b2d6932094d21ffbb6358f96
Moses Chukwunekwu <moses@omenai.net>
2026-02-14 19:53:15 +0100

    FEAT: Live migration

--- configs/url-config/src/config.ts
+++ configs/url-config/src/config.ts
@@ -1,7 +1,11 @@
 export const getApiUrl = (): string => {
-  return process.env.NODE_ENV === "production"
-    ? "https://staging.api.omenai.app"
-    : "http://localhost:8001";
+  if (process.env.APP_ENV === "production") {
+    return "https://api.omenai.app";
+  } else {
+    return process.env.NODE_ENV === "production"
+      ? "https://staging.api.omenai.app"
+      : "http://localhost:8001";
+  }
 };
 export const session_auth_url = (): string => {
   if (process.env.NODE_ENV === "development") {
@@ -11,30 +15,45 @@ export const session_auth_url = (): string => {
   }
 };
 export const auth_uri = (): string => {
-  return process.env.NODE_ENV === "production"
-    ? "https://staging.auth.omenai.app"
-    : "http://localhost:4000";
+  if (process.env.APP_ENV === "production") {
+    return "https://auth.omenai.app";
+  } else
+    return process.env.NODE_ENV === "production"
+      ? "https://staging.auth.omenai.app"
+      : "http://localhost:4000";
 };
 
 export const dashboard_url = (): string => {
-  return process.env.NODE_ENV === "production"
-    ? "https://staging.dashboard.omenai.app"
-    : "http://localhost:5000";
+  if (process.env.APP_ENV === "production") {
+    return "https://dashboard.omenai.app";
+  } else
+    return process.env.NODE_ENV === "production"
+      ? "https://staging.dashboard.omenai.app"
+      : "http://localhost:5000";
 };
 
 export const base_url = (): string => {
-  return process.env.NODE_ENV === "production"
-    ? "https://staging.omenai.app"
-    : "http://localhost:3000";
+  if (process.env.APP_ENV === "production") {
+    return "https://omenai.app";
+  } else
+    return process.env.NODE_ENV === "production"
+      ? "https://staging.omenai.app"
+      : "http://localhost:3000";
 };
 export const admin_url = (): string => {
-  return process.env.NODE_ENV === "production"
-    ? "https://staging.admin.omenai.app"
-    : "http://localhost:3001";
+  if (process.env.APP_ENV === "production") {
+    return "https://admin.omenai.app";
+  } else
+    return process.env.NODE_ENV === "production"
+      ? "https://staging.admin.omenai.app"
+      : "http://localhost:3001";
 };
 
 export const tracking_url = (): string => {
-  return process.env.NODE_ENV === "production"
-    ? "https://staging.tracking.omenai.app"
-    : "http://localhost:3002";
+  if (process.env.APP_ENV === "production") {
+    return "https://tracking.omenai.app";
+  } else
+    return process.env.NODE_ENV === "production"
+      ? "https://staging.tracking.omenai.app"
+      : "http://localhost:3002";
 };
