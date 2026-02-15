4db02ff5c7a9b303f7c67e8b548ba1f45526fd38
Moses Chukwunekwu <moses@omenai.net>
2026-02-15 12:52:28 +0100

    Fixes

--- configs/appwrite-config/appwrite.ts
+++ configs/appwrite-config/appwrite.ts
@@ -6,36 +6,45 @@ import {
   ID,
   TablesDB,
 } from "appwrite";
-import {
-  Client as ServerClient,
-  Storage as ServerStorage,
-} from "node-appwrite";
 
+// -----------------------------------------------------------------------------
+// 1. CLIENT-SIDE SETUP (Always runs)
+// -----------------------------------------------------------------------------
 const client = new Client();
-const serverClient = new Client();
 
-const serverEndpoint = process.env.APPWRITE_ENDPOINT as string;
-const clientEndPoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string;
-
-client
-  .setEndpoint(clientEndPoint)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_CLIENT_ID!);
-
-serverClient
-  .setEndpoint(serverEndpoint)
-  .setProject(process.env.APPWRITE_CLIENT_ID!);
-
-export const nodeAppwriteClient = new ServerClient()
-  .setEndpoint(serverEndpoint)
-  .setProject(process.env.APPWRITE_CLIENT_ID!)
-  .setKey(process.env.APPWRITE_STORAGE_API_KEY!);
-
-export const nodeAppwriteStorage = new ServerStorage(nodeAppwriteClient);
+if (process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT) {
+  client
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_CLIENT_ID!);
+}
 
 export const storage = new Storage(client);
-export const serverStorage = new Storage(serverClient);
-
 export const editorial_database = new TablesDB(client);
 export const appwrite_image_format = ImageFormat;
 export const appwrite_image_gravity = ImageGravity;
 export const identifier = ID;
+
+// -----------------------------------------------------------------------------
+// 2. SERVER-SIDE SETUP (Lazy Loaded)
+// -----------------------------------------------------------------------------
+
+let serverClient: any = null;
+let serverStorage: any = null;
+
+if (typeof window === "undefined") {
+  const { Client: NodeClient, Storage: NodeStorage } = require("node-appwrite");
+
+  const internalClient = new NodeClient();
+
+  // Use the PRIVATE server variables here
+  internalClient
+    .setEndpoint(process.env.APPWRITE_ENDPOINT!)
+    .setProject(process.env.APPWRITE_CLIENT_ID!)
+    .setKey(process.env.APPWRITE_STORAGE_API_KEY!);
+
+  serverClient = internalClient;
+  serverStorage = new NodeStorage(internalClient);
+}
+
+export const nodeAppwriteClient = serverClient;
+export const nodeAppwriteStorage = serverStorage;
