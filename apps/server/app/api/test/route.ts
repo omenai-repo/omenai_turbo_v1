fc1fd0c61d56b2230a08ff607b0a5307139de96c
rodolphe-o <rodolphe@omenai.net>
2026-03-03 10:41:37 -0500

    Update: waitlist emails

--- apps/server/app/api/test/route.ts
+++ apps/server/app/api/test/route.ts
@@ -11,6 +11,12 @@ import { sendPaymentSuccessMailArtist } from "@omenai/shared-emails/src/models/p
 import { sendArtistWaitlistInviteEmail } from "@omenai/shared-emails/src/models/admin/sendArtistWaitlistInviteEmail";
 import { sendCollectorWaitlistInvite } from "@omenai/shared-emails/src/models/admin/sendCollectorWaitlistInvite";
 import { getFutureShipmentDate } from "@omenai/shared-utils/src/getFutureShipmentDate";
+import { email } from "zod";
+import { render } from "@react-email/render";
+import SendArtistWaitListInvites from "@omenai/shared-emails/src/views/admin/SendArtistWaitListInvites";
+import SendCollectorWaitlistInvite from "@omenai/shared-emails/src/views/admin/SendCollectorWaitlistInvite";
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
 const payload = {
   email: "dantereus1@gmail.com",
   name: "Elias",
@@ -22,15 +28,61 @@ const payload = {
   order_date: "February 24, 2026",
   order_id: "882194-ACQ",
 };
-export async function GET() {
-  await sendArtistWaitlistInviteEmail({
+const matchedUsers = [
+  {
+    entity: "artist",
+    email: "rodolphe@omenai.net",
     name: "Theon Greyjoy",
-    email: "moses@omenai.net",
-  });
-  await sendCollectorWaitlistInvite({
+  },
+  {
+    entity: "user",
+    email: "rodolphe@omenai.net",
     name: "Lord Bolton",
+  },
+  {
+    entity: "artist",
     email: "moses@omenai.net",
-  });
+    name: "Theon Greyjoy",
+  },
+  {
+    entity: "user",
+    email: "moses@omenai.net",
+    name: "Lord Bolton",
+  },
+  {
+    entity: "artist",
+    email: "kelvin@omenai.net",
+    name: "Theon Greyjoy",
+  },
+  {
+    entity: "user",
+    email: "kelvin@omenai.net",
+    name: "Lord Bolton",
+  },
+];
+export async function GET() {
+  const inviteUserEmailPayload = await Promise.all(
+    matchedUsers.map(async (user) => {
+      if (user.entity === "artist") {
+        const html = await render(SendArtistWaitListInvites(user.name));
+        return {
+          from: "Omenai Onboarding <onboarding@omenai.app>",
+          to: [user.email],
+          subject: "OMENAI is Live — Activate Your Profile",
+          html,
+        };
+      } else {
+        const html = await render(SendCollectorWaitlistInvite(user.name));
+        return {
+          from: "Omenai Onboarding <onboarding@omenai.app>",
+          to: [user.email],
+          subject: "OMENAI is Now Live",
+          html,
+        };
+      }
+    }),
+  );
+  await resend.batch.send(inviteUserEmailPayload);
 
   // const data = await getFutureShipmentDate(3, true, "US");
   return NextResponse.json({ message: "Test route is working!" });
--- shared/shared-emails/src/views/admin/SendCollectorWaitlistInvite.tsx
+++ shared/shared-emails/src/views/admin/SendCollectorWaitlistInvite.tsx
@@ -1,5 +1,6 @@
 import { Img, Link, Section, Text } from "@react-email/components";
 import AdminEmailLayout from "./AdminEmailLayout";
+import DownloadAppSection from "./DownloadAppSection";
 
 const SendCollectorWaitlistInvite = (name: string) => {
   return (
@@ -39,79 +40,7 @@ const SendCollectorWaitlistInvite = (name: string) => {
           Create Your Account
         </Link>
       </div>
-      <Section
-        style={{
-          width: "100%",
-          textAlign: "center",
-        }}
-      >
-        <Text
-          className="m-0 text-fluid-xxs font-semibold leading-[32px] text-gray-900"
-          style={{ textAlign: "center" }}
-        >
-          Or download the app
-        </Text>
-        <table width="100%" cellPadding="0" cellSpacing="0" border={0}>
-          <thead style={{ display: "none" }}>
-            <tr>
-              <th>App Store Download Links</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td align="center">
-                <table
-                  cellPadding="0"
-                  cellSpacing="0"
-                  border={0}
-                  style={{ display: "inline-block" }}
-                >
-                  <thead style={{ display: "none" }}>
-                    <tr>
-                      <th>App Store</th>
-                      <th>Google Play</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td style={{ paddingRight: "16px" }}>
-                        <Link href="https://apps.apple.com/app/6748387089">
-                          <Img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/320px-Download_on_the_App_Store_Badge.svg.png"
-                            alt="Download on the App Store"
-                            width="135"
-                            height="40"
-                            style={{
-                              display: "block",
-                              border: "0",
-                              outline: "none",
-                            }}
-                          />
-                        </Link>
-                      </td>
-                      <td style={{ paddingLeft: "16px" }}>
-                        <Link href="https://play.google.com/store/apps/details?id=com.omenai.omenaiapp">
-                          <Img
-                            src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-                            alt="Download on the Google Play"
-                            width="160"
-                            height="62"
-                            style={{
-                              display: "block",
-                              border: "0",
-                              outline: "none",
-                            }}
-                          />
-                        </Link>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </Section>
+      <DownloadAppSection />
       <br />
       <Text
         className="footer-text-main"
